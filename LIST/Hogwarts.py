86c201017ecc0b1b814f71499ff597d60b905d2d
Salau Ibrahim <officialsalau@gmail.com>
2024-05-02 17:33:35 +0100

    Anothe commit

--- LIST/Hogwarts.py
+++ LIST/Hogwarts.py
@@ -1,8 +1,21 @@
 #Working with LIST using for loop
 #Print out the students one after the other
+# def main():
+#     students = ["Salau", "Sarinat", "Oye"]
+#     for student in range(len(students)):
+#         print(student + 1, students[student])
+
+# main()
+
+
+#DICTIONARY
 def main():
-    students = ["Salau", "Sarinat", "Oye"]
-    for student in range(len(students)):
-        print(student + 1, students[student])
+    students = {"Salau": "Lagos",
+                "Sarinat": "Lagos",
+                "Oye": "Lagos",
+                "Balogun": "Others"}
+    
+    for student in students:
+        print(student, students[student], sep=", ")
 
 main()

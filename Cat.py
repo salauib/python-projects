e608d003c9ca3aef90f6981e97265345495b57fd
Salau Ibrahim <officialsalau@gmail.com>
2024-05-02 14:31:41 +0100

    Another commit

--- Cat.py
+++ Cat.py
@@ -18,13 +18,19 @@
 
 # cat()
 
-def cat():
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
     while True:
-        n = int(input("What's n? \n"))
+        n = int(input("What's n? "))
         if n > 0:
             break
+    return n    
 
+def meow(n):
     for _ in range(n):
         print("meow")
 
-cat()
+main()

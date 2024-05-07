1d666cb59aee46905bd3e87be5640cdec69bfd3f
Salau Ibrahim <officialsalau@gmail.com>
2024-05-07 22:13:01 +0100

    Downloader code rearanged

--- YouTube Video Downloader.py
+++ YouTube Video Downloader.py
@@ -1,9 +1,8 @@
-
 from pytube import YouTube
+
 def main():
     link = input("Enter youtube video URL?\n ")
     video = YouTube(link)
     stream = video.streams.get_highest_resolution()
     stream.download()
-
 main()

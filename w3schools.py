c406ed26eb0a61fadaae9981b690cbe732a57f9c
Salau Ibrahim <officialsalau@gmail.com>
2024-05-07 23:35:07 +0100

    Setting and getting data types

--- w3schools.py
+++ w3schools.py
@@ -1,18 +1,54 @@
-#CREATED GLOBAL AND LOCAL VARIABLES
-#Global variale
-globalvariable = input("Enter your name please?\n ").capitalize()
+# #CREATED GLOBAL AND LOCAL VARIABLES
+# #Global variale
+# globalvariable = input("Enter your name please?\n ").capitalize()
+
+# def main():
+#     #Local variable
+#     localVariable = "python programmer"
+
+#     #Making a local variable glocal with keyword "global"
+#     global x
+#     x = "That's right!"
+
+#     print(globalvariable + " is a  " + localVariable)
+# main()
+
+# print(globalvariable)
+# print(x)
+
+#Data Types
+#Setting and getting data types
 
 def main():
-    #Local variable
-    localVariable = "python programmer"
+    a = "Salau"
+    b = 35
+    c = 20.3
+    d = 2j
+    e = ["mango", "orange"]
+    f = ("mango", "orange")
+    g = range(3)
+    h = {"name": "Salau", "age": 35,}
+    i = {"man", "woman"}
+    j = frozenset({"apple", "berry"})
+    k = True
+    l = b"Hello"
+    m = bytearray(7)
+    n = memoryview(bytes(7))
+    o = None
 
-    #Making a local variable glocal with keyword "global"
-    global x
-    x = "That's right!"
-
-    print(globalvariable + " is a  " + localVariable)
+    print(type(a))
+    print(type(b))
+    print(type(c))
+    print(type(d))
+    print(type(e))
+    print(type(f))
+    print(type(g))
+    print(type(h))
+    print(type(i))
+    print(type(j))
+    print(type(k))
+    print(type(l))
+    print(type(m))
+    print(type(n))
+    print(type(o))
 main()
-
-print(globalvariable)
-print(x)
-

a6a570e6949384ce187143032b594b0fd4e7b172
Salau Ibrahim <officialsalau@gmail.com>
2024-05-07 23:12:54 +0100

    Another commit

--- w3schools.py
+++ w3schools.py
@@ -1,3 +1,4 @@
+#CREATED GLOBAL AND LOCAL VARIABLES
 #Global variale
 globalvariable = input("Enter your name please?\n ").capitalize()
 
@@ -14,3 +15,4 @@ main()
 
 print(globalvariable)
 print(x)
+

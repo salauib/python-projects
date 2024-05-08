d0a8e09a7eae737c72d5ab20ec76417c7a9c88d1
Salau Ibrahim <officialsalau@gmail.com>
2024-05-08 23:40:39 +0100

    Another commit

--- List.py
+++ List.py
@@ -2,9 +2,38 @@
 def main():
     myList = ["Apple", "Mango", "Guava", "mango"]
     myList2 = [1, 2, 3, 4, 8, 26, 5]
+    myList22 =[6, 7, 22, 33, 4, 00, 0, 10]
+    myTuple = ("Kiwi", "Orange")
     myList3 = [True, False, False]
     myList4 = ["abc", 34, True, 40, "male"]
-    myList5 = list(("Guava", "Berry", "Broccoli", "Cashew"))
+    myList5 = list(("Guava", "Berry", "Broccoli", "Cashew", "Orange", "Melon", "Banana"))
+
+    #Add a list to anothe list using extend method
+    myList2.extend(myList22)
+
+    #You can add an iterable data type to list eg Tuple
+    myList2.extend(myTuple)
+
+    #Change a specific item value in a list
+    myList3[2] = "True"
+    myList4[1:3] = [35, False]
+
+    #Insert an item into a list
+    myList4.insert(4, "she")
+    myList4.append("them")
+
+    #Remove an item from a list using remove() method
+    myList2.remove("Orange")
+    myList2.remove(0)
+
+    #Remove a specific item from a list using pop() method
+    myList2.pop(12)
+
+    #Check if an item is in a list
+    if True in myList3:
+        print("Yes!")
+    else:
+        print("No!")
 
     print(len(myList)) #List length
     print(len(myList[1])) #Length of list's item number 2
@@ -13,6 +42,6 @@ def main():
     print(myList2)
     print(myList3)
     print(myList4)
-    print(myList4[:2])
-    print(myList5[-3])
+    print(myList4[-3:-1])
+    print(myList5[2:5])
 main()

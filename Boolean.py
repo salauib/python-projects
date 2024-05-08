c536f0ed80be43d6e7a6841aaf074bc07cc4c6fa
Salau Ibrahim <officialsalau@gmail.com>
2024-05-08 17:52:52 +0100

    Boolean.py initiated

---
+++ Boolean.py
@@ -0,0 +1,33 @@
+# def myBoolean():
+#     x = "Name"
+#     y = 55
+#     print(10 >= 9)
+#     print(bool(5))
+#     print(bool("Yes"))
+#     print(bool(x == "Age"))
+#     print(bool(y >= 56))
+#     print(bool(0))
+#     print(bool(""))
+#     print(bool([]))
+#     print(bool({}))
+#     print(bool(()))
+#     print(bool(None))
+#     print(bool(False))
+#     print(bool(True))
+# myBoolean()
+
+# #A function that returns a boolean value
+# def main():
+#     return True
+
+# if main():
+#     print("YES!")
+# else:
+#     print("NO!")
+# print(main())
+
+#Check if an object is an integer or not using isinstance() function
+def isAnInteger():
+    x = "Letter"
+    print(isinstance(x, int))
+isAnInteger()

7cb872c2c85061d5c2c2fc8c6d2c52bfa22391f2
Salau Ibrahim <officialsalau@gmail.com>
2024-05-10 16:45:43 +0100

    Printed first tuple

---
+++ Tuples.py
@@ -0,0 +1,8 @@
+#A tuple is a collection of data which is ordered and unchangeable
+#Create a Tuple
+def main():
+    aTuple = ("apple", "mango", "orange")
+
+    print(aTuple)
+    print(len(aTuple))
+results = main()

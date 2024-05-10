752ede89d81ac27ebf416da65e895b79f72f6eca
Salau Ibrahim <officialsalau@gmail.com>
2024-05-10 19:10:46 +0100

    some operations on tuple

--- Tuples.py
+++ Tuples.py
@@ -1,3 +1,4 @@
+####################################################
 #A tuple is a collection of data which is ordered and unchangeable
 #Create a Tuple
 def main():
@@ -6,3 +7,29 @@ def main():
     print(aTuple)
     print(len(aTuple))
 results = main()
+
+#####################################
+#One item Tuple, don't miss the last comma (,)
+def main():
+    aTuple = ("apple",)
+
+    print(type(aTuple))
+    print(len(aTuple))
+results = main()
+
+#####################################
+#String, int and boolean data type
+def main():
+    thisTuple1 = ("apple", "mango", "cherry")
+    thisTuple2 = (1, 2, 3, 4, 5)
+    thisTuple3 = (True, False, False)
+    thisTuple4 =("abc", 34, 4, True, False, 'good')
+
+    print(type(thisTuple1))
+    print(type(thisTuple2))
+    print(type(thisTuple3))
+    print(type(thisTuple4))
+    print(thisTuple4[3])
+
+answer = main()
+

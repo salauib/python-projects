752c2a75413ebe3deec8fdf16b0efe965839d681
Salau Ibrahim <officialsalau@gmail.com>
2024-05-11 22:48:31 +0100

    Few more codes added

--- Set.py
+++ Set.py
@@ -13,3 +13,23 @@ def main():
     print(type(yourset))
     print(type(x))
 main()
+
+######################
+#Note: True and 1 is considered the same value in set
+#False and 0 are also the same
+def main():
+    thisset = {"apple", False, "banana", "cherry", True, 1, 2, 1, 0}
+    
+    print(thisset)
+    print(len(thisset))
+    print(type(thisset))
+main()
+
+##################
+#Using set() constructor to create set
+def main():
+    thisset = set(('apple', 'mango', 'pineapple'))
+
+    print(thisset)
+    print(type(thisset))
+main()

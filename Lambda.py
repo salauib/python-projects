d88eeb7209de6058f2b40d6c432a7cb3186c5913
Salau Ibrahim <officialsalau@gmail.com>
2024-05-14 14:20:48 +0100

    Another commit

--- Lambda.py
+++ Lambda.py
@@ -9,6 +9,6 @@ lambdaFunction()
 ###################################
 #Lambda can take multiple arguments
 def lambdaFunction():
-    x = lambda a, b : a * b
-    print(x(5,6))
+    x = lambda a, b, c : (a * b) + c
+    print(x(5, 6, 3))
 lambdaFunction()
